d6651bdca81e812867cfde4099424ff7398080b7
sovitchaudhary <sovitchaudhary45@gmail.com>
2023-08-09 20:54:24 +0530

    minor change

--- admin/login.php
+++ admin/login.php
@@ -32,7 +32,7 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href=".css/styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
     <div class="wrapper fadeInDown">
