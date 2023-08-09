a4a1fcad77da44c131e14c75883f493c9a6f7b12
sovitchaudhary <sovitchaudhary45@gmail.com>
2023-08-09 20:36:11 +0530

    Added Admin dashboard and it's functionality

---
+++ admin/login.php
@@ -0,0 +1,69 @@
+<?php
+	session_start();
+	include('../includes/db.php');
+	if(isset($_POST['adminLogin'])) {
+        $query= "SELECT `uid`, `username`, `email`, `password` FROM `admin` WHERE email = '$_POST[email]' AND password = '$_POST[password]'";
+		$query_run = mysqli_query($connection,$query);
+		if(mysqli_num_rows($query_run)){
+			while($row = mysqli_fetch_assoc($query_run)){
+				$_SESSION['email'] = $row['email'];
+				$_SESSION['name'] = $row['name'];
+
+			}
+			echo "<script type='text/javascript'>
+			window.location.href = 'admin_dashboard.php';
+			</script>";
+		}
+		else{
+			echo "<script type='text/javascript'>
+			alert('Please enter correct email and password!');
+			window.location.href = 'login.php';
+			</script>";
+		}
+	}
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href=".css/styles.css">
+</head>
+<body>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <!-- Tabs Titles -->
+
+            <!-- Icon -->
+            <div>
+                <h4 style="padding: top 10px;">Sign In</h4>
+            </div>
+
+            <!-- Login Form -->
+            <form action="" method="POST">
+                <div>
+                    <input type="email" id="login" class="fadeIn second" name="email" placeholder="Email" required>
+                </div>
+                <div>
+                    <input type="password" id="password" class="fadeIn third" name="password" placeholder="password"
+                        required>
+                </div>
+
+                <input type="submit" name="adminLogin" class="fadeIn fourth" value="Log In">
+                <div>
+                    Go back <a class="underlineHover" href="../index.php">Home</a>
+                </div>
+                <br>
+            </form>
+
+
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ admin/manage_task.php
@@ -0,0 +1,64 @@
+<?php
+	session_start();
+	if (isset($_SESSION['email'])) {
+	include('../includes/db.php');
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Admin dashboard</title>
+		<!-- jQuery file -->
+		<script src="../js/jquery.js"></script>
+		<!-- Bootstrap files -->
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+		<script src="../js/bootstrap.min.js"></script>
+		<!-- External css file -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/new.css">
+	</head>
+
+<body>
+	<center><h2>All assigned tasks</h2></center>
+	<table class="table" style="background-color: black; widows: 80vw;">
+		<tr>
+			<th>S.No.</th>
+			<th>Task ID</th>
+			<th>Description</th>
+			<th>Start Date</th>
+			<th>End Date</th>
+			<th>Status</th>
+			<th>Action</th>
+		</tr>
+		<?php
+		    $sno = 1;
+			$query = "select *from tasks";
+			$query_run = mysqli_query($connection,$query);
+			while($row = mysqli_fetch_assoc($query_run)){
+				?>
+				<tr style="background-color: #f9d1d1;">
+					<td><?php echo $sno; ?></td>
+					<td><?php echo $row['tid']; ?></td>
+					<td><?php echo $row['description']; ?></td>
+					<td><?php echo $row['start_date']; ?></td>
+					<td><?php echo $row['end_date']; ?></td>
+					<td><?php echo $row['status']; ?></td>
+					<td><a href="edit_task.php?id=<?php echo $row['tid']; ?>">Edit</a> | <a href="delete_task.php?id=<?php echo $row['tid']; ?>">Delete</a></td>
+				</tr>
+				<?php
+				$sno = $sno + 1;
+			} 	
+		 ?>
+	</table>
+
+</body>
+</html>
+<?php
+	}
+	else{
+		header('Location: admin_login.php');
+	}
+?>
