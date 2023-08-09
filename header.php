591872ac27189b3aefdbb2b37789eba0328050e4
sovitchaudhary <sovitchaudhary45@gmail.com>
2023-08-09 19:41:35 +0530

    Design first Page

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+    include "includes/db.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include "header.php";
+?>
+
+<body>
+    <div class="wrapper fadeInDown">
+        <div>
+        <h3 style="background-color: Green;" >Welcome To Project Management TOOL</h3>
+        </div>
+        <div>
+            <b>User Login</b> 
+        </div>
+        <a href="login.php">login</a>
+        <div class="wrapper fadeInDown">
+        <div>
+            <b>Admin Login</b>  
+        </div>
+        <a href="admin/login.php">login</a>
+
+    </div>
+</body>
+
+</html>
